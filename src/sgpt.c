211fdc6a4937b503f36037c5310f4b290dbae0a9
Takuya Kato <Ktakuya332C@users.noreply.github.com>
2023-05-17 21:06:37 +0900

    apply clang-format

--- src/sgpt.c
+++ src/sgpt.c
@@ -1,64 +1,55 @@
 #include "sgpt.h"
+
 #include <assert.h>
 
 static const size_t SGPT_TYPE_SIZE[SGPT_TYPE_COUNT] = {
-  [SGPT_TYPE_F32] = sizeof(float),
-  [SGPT_TYPE_I32] = sizeof(int32_t),
+    [SGPT_TYPE_F32] = sizeof(float),
+    [SGPT_TYPE_I32] = sizeof(int32_t),
 };
 
 static sgpt_context ctx;
 sgpt_context* sgpt_init(sgpt_init_params params) {
   ctx = (sgpt_context){
-    .mem_size = params.mem_size,
-    .mem_buffer = params.mem_buffer,
-    .n_objects = 0,
-    .objects_begin = NULL,
-    .objects_end = NULL,
+      .mem_size = params.mem_size,
+      .mem_buffer = params.mem_buffer,
+      .n_objects = 0,
+      .objects_begin = NULL,
+      .objects_end = NULL,
   };
   return &ctx;
 }
 
-static inline bool sgpt_are_same_shape(
-  const sgpt_tensor* a,
-  const sgpt_tensor* b
-) {
+static inline bool sgpt_are_same_shape(const sgpt_tensor* a,
+                                       const sgpt_tensor* b) {
   static_assert(SGPT_MAX_DIMS == 4, "SPGT_MAX_DIMS != 4");
-  return (
-    (a->ne[0] == b->ne[0]) &&
-    (a->ne[1] == b->ne[1]) &&
-    (a->ne[2] == b->ne[2]) &&
-    (a->ne[3] == b->ne[3])
-  );
+  return ((a->ne[0] == b->ne[0]) && (a->ne[1] == b->ne[1]) &&
+          (a->ne[2] == b->ne[2]) && (a->ne[3] == b->ne[3]));
 }
 
-static sgpt_tensor* sgpt_new_tensor_impl(
-  sgpt_context* ctx,
-  sgpt_type type,
-  int n_dims,
-  const int64_t* ne,
-  void* data
-) {
+static sgpt_tensor* sgpt_new_tensor_impl(sgpt_context* ctx, sgpt_type type,
+                                         int n_dims, const int64_t* ne,
+                                         void* data) {
   sgpt_object* obj_cur = ctx->objects_end;
   const size_t cur_offset = obj_cur == NULL ? 0 : obj_cur->offset;
   const size_t cur_size = obj_cur == NULL ? 0 : obj_cur->size;
   const size_t cur_end = cur_offset + cur_size;
   char* const mem_buffer = ctx->mem_buffer;
-  
+
   size_t size_needed = 0;
   if (data == NULL) {
     size_needed += SGPT_TYPE_SIZE[type];
-    for (int i=0; i<n_dims; i++) {
+    for (int i = 0; i < n_dims; i++) {
       size_needed *= ne[i];
     }
   }
   size_needed += sizeof(sgpt_tensor);
   assert(cur_end + sizeof(sgpt_object) + size_needed <= ctx->mem_size);
-  
+
   sgpt_object* const obj_new = (sgpt_object*)(mem_buffer + cur_end);
   *obj_new = (sgpt_object){
-    .offset = cur_end + sizeof(sgpt_object),
-    .size = size_needed,
-    .next = NULL,
+      .offset = cur_end + sizeof(sgpt_object),
+      .size = size_needed,
+      .next = NULL,
   };
   if (obj_cur == NULL) {
     ctx->objects_begin = obj_new;
@@ -66,74 +57,54 @@ static sgpt_tensor* sgpt_new_tensor_impl(
     obj_cur->next = obj_new;
   }
   ctx->objects_end = obj_new;
-  
+
   sgpt_tensor* const result = (sgpt_tensor*)(mem_buffer + obj_new->offset);
   *result = (sgpt_tensor){
-    .type = type,
-    .n_dims = n_dims,
-    .ne = { 1, 1, 1, 1 }, // placeholder
-    .nb = { 0, 0, 0, 0 }, // placeholder
-    .op = SGPT_OP_NONE,
-    .src0 = NULL,
-    .src1 = NULL,
-    .data = data == NULL ? (void*)(result+1) : data,
+      .type = type,
+      .n_dims = n_dims,
+      .ne = {1, 1, 1, 1},  // placeholder
+      .nb = {0, 0, 0, 0},  // placeholder
+      .op = SGPT_OP_NONE,
+      .src0 = NULL,
+      .src1 = NULL,
+      .data = data == NULL ? (void*)(result + 1) : data,
   };
-  for (int i=0; i<n_dims; i++) result->ne[i] = ne[i];
+  for (int i = 0; i < n_dims; i++) result->ne[i] = ne[i];
   result->nb[0] = SGPT_TYPE_SIZE[type];
   result->nb[1] = result->nb[0] * result->ne[0];
   result->nb[2] = result->nb[1] * result->ne[1];
   result->nb[3] = result->nb[2] * result->ne[2];
-  
+
   ctx->n_objects++;
-  
+
   return result;
 }
 
-static sgpt_tensor* sgpt_new_tensor(
-  sgpt_context* ctx,
-  sgpt_type type,
-  int n_dims,
-  const int64_t* ne
-) {
+static sgpt_tensor* sgpt_new_tensor(sgpt_context* ctx, sgpt_type type,
+                                    int n_dims, const int64_t* ne) {
   return sgpt_new_tensor_impl(ctx, type, n_dims, ne, NULL);
 }
 
-sgpt_tensor* sgpt_new_tensor_1d(
-  sgpt_context* ctx,
-  sgpt_type type, int64_t ne0) {
+sgpt_tensor* sgpt_new_tensor_1d(sgpt_context* ctx, sgpt_type type,
+                                int64_t ne0) {
   return sgpt_new_tensor(ctx, type, 1, &ne0);
 }
 
-sgpt_tensor* sgpt_new_tensor_2d(
-  sgpt_context* ctx,
-  sgpt_type type,
-  int64_t ne0,
-  int64_t ne1
-) {
-  const int64_t ne[2] = { ne0, ne1 };
+sgpt_tensor* sgpt_new_tensor_2d(sgpt_context* ctx, sgpt_type type, int64_t ne0,
+                                int64_t ne1) {
+  const int64_t ne[2] = {ne0, ne1};
   return sgpt_new_tensor(ctx, type, 2, ne);
 }
 
-sgpt_tensor* sgpt_new_tensor_3d(
-  sgpt_context* ctx,
-  sgpt_type type,
-  int64_t ne0,
-  int64_t ne1,
-  int64_t ne2
-) {
-  const int64_t ne[3] = { ne0, ne1, ne2 };
+sgpt_tensor* sgpt_new_tensor_3d(sgpt_context* ctx, sgpt_type type, int64_t ne0,
+                                int64_t ne1, int64_t ne2) {
+  const int64_t ne[3] = {ne0, ne1, ne2};
   return sgpt_new_tensor(ctx, type, 3, ne);
 }
 
-sgpt_tensor* sgpt_new_tensor_4d(
-  sgpt_context* ctx,
-  sgpt_type type,
-  int64_t ne0,
-  int64_t ne1,
-  int64_t ne2,
-  int64_t ne3
-) {
-  const int64_t ne[4] = { ne0, ne1, ne2, ne3 };
+sgpt_tensor* sgpt_new_tensor_4d(sgpt_context* ctx, sgpt_type type, int64_t ne0,
+                                int64_t ne1, int64_t ne2, int64_t ne3) {
+  const int64_t ne[4] = {ne0, ne1, ne2, ne3};
   return sgpt_new_tensor(ctx, type, 4, ne);
 }
 
@@ -148,7 +119,7 @@ sgpt_tensor* sgpt_view_tensor(sgpt_context* ctx, const sgpt_tensor* src) {
 int32_t sgpt_get_i32(const sgpt_tensor* tensor, int n_dims, const int* idxs) {
   assert(tensor->type == SGPT_TYPE_I32);
   int loc = 0;
-  for (int i=0; i<n_dims; i++) {
+  for (int i = 0; i < n_dims; i++) {
     assert(idxs[i] <= tensor->ne[i]);
     loc += idxs[i] * tensor->nb[i];
   }
@@ -160,24 +131,26 @@ int32_t sgpt_get_i32_1d(const sgpt_tensor* tensor, int i0) {
 }
 
 int32_t sgpt_get_i32_2d(const sgpt_tensor* tensor, int i0, int i1) {
-  const int idxs[2] = { i0, i1 };
+  const int idxs[2] = {i0, i1};
   return sgpt_get_i32(tensor, 2, idxs);
 }
 
 int32_t sgpt_get_i32_3d(const sgpt_tensor* tensor, int i0, int i1, int i2) {
-  const int idxs[3] = { i0, i1, i2 };
+  const int idxs[3] = {i0, i1, i2};
   return sgpt_get_i32(tensor, 3, idxs);
 }
 
-int32_t sgpt_get_i32_4d(const sgpt_tensor* tensor, int i0, int i1, int i2, int i3) {
-  const int idxs[4] = { i0, i1, i2, i3 };
+int32_t sgpt_get_i32_4d(const sgpt_tensor* tensor, int i0, int i1, int i2,
+                        int i3) {
+  const int idxs[4] = {i0, i1, i2, i3};
   return sgpt_get_i32(tensor, 4, idxs);
 }
 
-void sgpt_set_i32(const sgpt_tensor* tensor, int n_dims, const int* idxs, int32_t value) {
+void sgpt_set_i32(const sgpt_tensor* tensor, int n_dims, const int* idxs,
+                  int32_t value) {
   assert(tensor->type == SGPT_TYPE_I32);
   int loc = 0;
-  for (int i=0; i<n_dims; i++) {
+  for (int i = 0; i < n_dims; i++) {
     assert(idxs[i] <= tensor->ne[i]);
     loc += idxs[i] * tensor->nb[i];
   }
@@ -189,22 +162,26 @@ void sgpt_set_i32_1d(sgpt_tensor* tensor, int i0, int32_t value) {
 }
 
 void sgpt_set_i32_2d(sgpt_tensor* tensor, int i0, int i1, int32_t value) {
-  const int idxs[2] = { i0, i1 };
+  const int idxs[2] = {i0, i1};
   return sgpt_set_i32(tensor, 2, idxs, value);
 }
 
-void sgpt_set_i32_3d(sgpt_tensor* tensor, int i0, int i1, int i2, int32_t value) {
-  const int idxs[3] = { i0, i1, i2 };
+void sgpt_set_i32_3d(sgpt_tensor* tensor, int i0, int i1, int i2,
+                     int32_t value) {
+  const int idxs[3] = {i0, i1, i2};
   return sgpt_set_i32(tensor, 3, idxs, value);
 }
 
-void sgpt_set_i32_4d(sgpt_tensor* tensor, int i0, int i1, int i2, int i3, int32_t value) {
-  const int idxs[4] = { i0, i1, i2, i3 };
+void sgpt_set_i32_4d(sgpt_tensor* tensor, int i0, int i1, int i2, int i3,
+                     int32_t value) {
+  const int idxs[4] = {i0, i1, i2, i3};
   return sgpt_set_i32(tensor, 4, idxs, value);
 }
 
-static sgpt_tensor* sgpt_dup_impl(sgpt_context* ctx, sgpt_tensor* a, bool inplace) {
-  sgpt_tensor* result = inplace ? sgpt_view_tensor(ctx, a) : sgpt_dup_tensor(ctx, a);
+static sgpt_tensor* sgpt_dup_impl(sgpt_context* ctx, sgpt_tensor* a,
+                                  bool inplace) {
+  sgpt_tensor* result =
+      inplace ? sgpt_view_tensor(ctx, a) : sgpt_dup_tensor(ctx, a);
   result->op = SGPT_OP_DUP;
   result->src0 = a;
   result->src1 = NULL;
@@ -219,9 +196,11 @@ sgpt_tensor* sgpt_dup_inplace(sgpt_context* ctx, sgpt_tensor* a) {
   return sgpt_dup_impl(ctx, a, true);
 }
 
-static sgpt_tensor* sgpt_add_impl(sgpt_context* ctx, sgpt_tensor* a, sgpt_tensor* b, bool inplace) {
+static sgpt_tensor* sgpt_add_impl(sgpt_context* ctx, sgpt_tensor* a,
+                                  sgpt_tensor* b, bool inplace) {
   assert(sgpt_are_same_shape(a, b));
-  sgpt_tensor* result = inplace ? sgpt_view_tensor(ctx, a) : sgpt_dup_tensor(ctx, a);
+  sgpt_tensor* result =
+      inplace ? sgpt_view_tensor(ctx, a) : sgpt_dup_tensor(ctx, a);
   result->op = SGPT_OP_ADD;
   result->src0 = a;
   result->src1 = b;
@@ -232,15 +211,16 @@ sgpt_tensor* sgpt_add(sgpt_context* ctx, sgpt_tensor* a, sgpt_tensor* b) {
   return sgpt_add_impl(ctx, a, b, false);
 }
 
-sgpt_tensor* sgpt_add_inplace(sgpt_context* ctx, sgpt_tensor* a, sgpt_tensor* b) {
+sgpt_tensor* sgpt_add_inplace(sgpt_context* ctx, sgpt_tensor* a,
+                              sgpt_tensor* b) {
   return sgpt_add_impl(ctx, a, b, true);
 }
 
 static void sgpt_visit_parents(sgpt_cgraph* cgraph, sgpt_tensor* node) {
-  for (int i=0; i<cgraph->n_nodes; i++) {
+  for (int i = 0; i < cgraph->n_nodes; i++) {
     if (cgraph->nodes[i] == node) return;
   }
-  for (int i=0; i<cgraph->n_leafs; i++) {
+  for (int i = 0; i < cgraph->n_leafs; i++) {
     if (cgraph->leafs[i] == node) return;
   }
   if (node->src0) sgpt_visit_parents(cgraph, node->src0);
@@ -258,13 +238,13 @@ static void sgpt_visit_parents(sgpt_cgraph* cgraph, sgpt_tensor* node) {
 
 sgpt_cgraph sgpt_build_forward(sgpt_tensor* tensor) {
   sgpt_cgraph result = {
-    .n_nodes = 0,
-    .n_leafs = 0,
-    .nodes = { NULL },
-    .leafs = { NULL },
+      .n_nodes = 0,
+      .n_leafs = 0,
+      .nodes = {NULL},
+      .leafs = {NULL},
   };
   sgpt_visit_parents(&result, tensor);
-  if (result.n_nodes > 0) assert(result.nodes[result.n_nodes-1] == tensor);
+  if (result.n_nodes > 0) assert(result.nodes[result.n_nodes - 1] == tensor);
   return result;
 }
 
@@ -274,13 +254,13 @@ static void sgpt_compute_forward_dup_f32(sgpt_tensor* src0, sgpt_tensor* dst) {
   assert(src0->ne[1] == dst->ne[1]);
   assert(src0->ne[2] == dst->ne[2]);
   assert(src0->ne[3] == dst->ne[3]);
-  for (int i3=0; i3<src0->ne[3]; i3++) {
+  for (int i3 = 0; i3 < src0->ne[3]; i3++) {
     size_t loc3 = src0->nb[3] * i3;
-    for (int i2=0; i2<src0->ne[2]; i2++) {
+    for (int i2 = 0; i2 < src0->ne[2]; i2++) {
       size_t loc2 = loc3 + src0->nb[2] * i2;
-      for (int i1=0; i1<src0->ne[1]; i1++) {
+      for (int i1 = 0; i1 < src0->ne[1]; i1++) {
         size_t loc1 = loc2 + src0->nb[1] * i1;
-        for (int i0=0; i0<src0->ne[0]; i0++) {
+        for (int i0 = 0; i0 < src0->ne[0]; i0++) {
           size_t loc0 = loc1 + src0->nb[0] * i0;
           ((float*)(dst->data + loc0))[0] = ((float*)(src0->data + loc0))[0];
         }
@@ -295,15 +275,16 @@ static void sgpt_compute_forward_dup_i32(sgpt_tensor* src0, sgpt_tensor* dst) {
   assert(src0->ne[1] == dst->ne[1]);
   assert(src0->ne[2] == dst->ne[2]);
   assert(src0->ne[3] == dst->ne[3]);
-  for (int i3=0; i3<src0->ne[3]; i3++) {
+  for (int i3 = 0; i3 < src0->ne[3]; i3++) {
     size_t loc3 = src0->nb[3] * i3;
-    for (int i2=0; i2<src0->ne[2]; i2++) {
+    for (int i2 = 0; i2 < src0->ne[2]; i2++) {
       size_t loc2 = loc3 + src0->nb[2] * i2;
-      for (int i1=0; i1<src0->ne[1]; i1++) {
+      for (int i1 = 0; i1 < src0->ne[1]; i1++) {
         size_t loc1 = loc2 + src0->nb[1] * i1;
-        for (int i0=0; i0<src0->ne[0]; i0++) {
+        for (int i0 = 0; i0 < src0->ne[0]; i0++) {
           size_t loc0 = loc1 + src0->nb[0] * i0;
-          ((int32_t*)(dst->data + loc0))[0] = ((int32_t*)(src0->data + loc0))[0];
+          ((int32_t*)(dst->data + loc0))[0] =
+              ((int32_t*)(src0->data + loc0))[0];
         }
       }
     }
@@ -324,7 +305,8 @@ static void sgpt_compute_forward_dup(sgpt_tensor* src0, sgpt_tensor* dst) {
   }
 }
 
-static void sgpt_compute_forward_add_f32(sgpt_tensor* src0, sgpt_tensor* src1, sgpt_tensor* dst) {
+static void sgpt_compute_forward_add_f32(sgpt_tensor* src0, sgpt_tensor* src1,
+                                         sgpt_tensor* dst) {
   static_assert(SGPT_MAX_DIMS == 4, "SPGT_MAX_DIMS != 4");
   assert(src0->ne[0] == dst->ne[0]);
   assert(src0->ne[1] == dst->ne[1]);
@@ -334,13 +316,13 @@ static void sgpt_compute_forward_add_f32(sgpt_tensor* src0, sgpt_tensor* src1, s
   assert(src1->ne[1] == dst->ne[1]);
   assert(src1->ne[2] == dst->ne[2]);
   assert(src1->ne[3] == dst->ne[3]);
-  for (int i3=0; i3<src0->ne[3]; i3++) {
+  for (int i3 = 0; i3 < src0->ne[3]; i3++) {
     size_t loc3 = src0->nb[3] * i3;
-    for (int i2=0; i2<src0->ne[2]; i2++) {
+    for (int i2 = 0; i2 < src0->ne[2]; i2++) {
       size_t loc2 = loc3 + src0->nb[2] * i2;
-      for (int i1=0; i1<src0->ne[1]; i1++) {
+      for (int i1 = 0; i1 < src0->ne[1]; i1++) {
         size_t loc1 = loc2 + src0->nb[1] * i1;
-        for (int i0=0; i0<src0->ne[0]; i0++) {
+        for (int i0 = 0; i0 < src0->ne[0]; i0++) {
           size_t loc0 = loc1 + src0->nb[0] * i0;
           ((float*)(dst->data + loc0))[0] = ((float*)(src0->data + loc0))[0];
           ((float*)(dst->data + loc0))[0] += ((float*)(src1->data + loc0))[0];
@@ -350,7 +332,8 @@ static void sgpt_compute_forward_add_f32(sgpt_tensor* src0, sgpt_tensor* src1, s
   }
 }
 
-static void sgpt_compute_forward_add_i32(sgpt_tensor* src0, sgpt_tensor* src1, sgpt_tensor* dst) {
+static void sgpt_compute_forward_add_i32(sgpt_tensor* src0, sgpt_tensor* src1,
+                                         sgpt_tensor* dst) {
   static_assert(SGPT_MAX_DIMS == 4, "SPGT_MAX_DIMS != 4");
   assert(src0->ne[0] == dst->ne[0]);
   assert(src0->ne[1] == dst->ne[1]);
@@ -360,23 +343,26 @@ static void sgpt_compute_forward_add_i32(sgpt_tensor* src0, sgpt_tensor* src1, s
   assert(src1->ne[1] == dst->ne[1]);
   assert(src1->ne[2] == dst->ne[2]);
   assert(src1->ne[3] == dst->ne[3]);
-  for (int i3=0; i3<src0->ne[3]; i3++) {
+  for (int i3 = 0; i3 < src0->ne[3]; i3++) {
     size_t loc3 = src0->nb[3] * i3;
-    for (int i2=0; i2<src0->ne[2]; i2++) {
+    for (int i2 = 0; i2 < src0->ne[2]; i2++) {
       size_t loc2 = loc3 + src0->nb[2] * i2;
-      for (int i1=0; i1<src0->ne[1]; i1++) {
+      for (int i1 = 0; i1 < src0->ne[1]; i1++) {
         size_t loc1 = loc2 + src0->nb[1] * i1;
-        for (int i0=0; i0<src0->ne[0]; i0++) {
+        for (int i0 = 0; i0 < src0->ne[0]; i0++) {
           size_t loc0 = loc1 + src0->nb[0] * i0;
-          ((int32_t*)(dst->data + loc0))[0] = ((int32_t*)(src0->data + loc0))[0];
-          ((int32_t*)(dst->data + loc0))[0] += ((int32_t*)(src1->data + loc0))[0];
+          ((int32_t*)(dst->data + loc0))[0] =
+              ((int32_t*)(src0->data + loc0))[0];
+          ((int32_t*)(dst->data + loc0))[0] +=
+              ((int32_t*)(src1->data + loc0))[0];
         }
       }
     }
   }
 }
 
-static void sgpt_compute_forward_add(sgpt_tensor* src0, sgpt_tensor* src1, sgpt_tensor* dst) {
+static void sgpt_compute_forward_add(sgpt_tensor* src0, sgpt_tensor* src1,
+                                     sgpt_tensor* dst) {
   assert(src0->type == dst->type);
   assert(src1->type == dst->type);
   switch (src0->type) {
@@ -407,7 +393,7 @@ static void sgpt_compute_forward(sgpt_tensor* tensor) {
 }
 
 void sgpt_graph_compute(sgpt_context* ctx, sgpt_cgraph* cgraph) {
-  for (int i=0; i<cgraph->n_nodes; i++) {
+  for (int i = 0; i < cgraph->n_nodes; i++) {
     sgpt_compute_forward(cgraph->nodes[i]);
   }
 }
--- tests/sgpt_test.c
+++ tests/sgpt_test.c
@@ -1,11 +1,12 @@
-#include "acutest.h"
 #include "sgpt.h"
+
+#include "acutest.h"
 
 void test_init(void) {
   uint8_t mem_buffer[8];
   const sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 8,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 8,
+      .mem_buffer = (void*)mem_buffer,
   });
   TEST_CHECK(ctx->mem_size == 8);
   TEST_CHECK(ctx->mem_buffer == mem_buffer);
@@ -17,8 +18,8 @@ void test_init(void) {
 void test_new_tensor(void) {
   uint8_t mem_buffer[4096];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 4096,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 4096,
+      .mem_buffer = (void*)mem_buffer,
   });
 
   const sgpt_tensor* a1 = sgpt_new_tensor_1d(ctx, SGPT_TYPE_F32, 4);
@@ -69,29 +70,29 @@ void test_new_tensor(void) {
 void test_set_i32(void) {
   uint8_t mem_buffer[4096];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 4096,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 4096,
+      .mem_buffer = (void*)mem_buffer,
   });
-  
+
   int32_t value = 0;
-  
+
   sgpt_tensor* a1 = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_set_i32_1d(a1, 0, 2);
   sgpt_set_i32_1d(a1, 1, 3);
   TEST_CHECK(sgpt_get_i32_1d(a1, 0) == 2);
   TEST_CHECK(sgpt_get_i32_1d(a1, 1) == 3);
-  
+
   sgpt_tensor* a2 = sgpt_new_tensor_2d(ctx, SGPT_TYPE_I32, 2, 3);
   value = 0;
-  for (int i=0; i<2; i++) {
-    for (int j=0; j<3; j++) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
       value++;
       sgpt_set_i32_2d(a2, i, j, value);
     }
   }
   value = 0;
-  for (int i=0; i<2; i++) {
-    for (int j=0; j<3; j++) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
       value++;
       TEST_CHECK(sgpt_get_i32_2d(a2, i, j) == value);
     }
@@ -99,18 +100,18 @@ void test_set_i32(void) {
 
   sgpt_tensor* a3 = sgpt_new_tensor_3d(ctx, SGPT_TYPE_I32, 2, 3, 5);
   value = 0;
-  for (int i=0; i<2; i++) {
-    for (int j=0; j<3; j++) {
-      for (int k=0; k<5; k++) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 5; k++) {
         value++;
         sgpt_set_i32_3d(a3, i, j, k, value);
       }
     }
   }
   value = 0;
-  for (int i=0; i<2; i++) {
-    for (int j=0; j<3; j++) {
-      for (int k=0; k<5; k++) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 5; k++) {
         value++;
         TEST_CHECK(sgpt_get_i32_3d(a3, i, j, k) == value);
       }
@@ -119,10 +120,10 @@ void test_set_i32(void) {
 
   sgpt_tensor* a4 = sgpt_new_tensor_4d(ctx, SGPT_TYPE_I32, 2, 3, 5, 7);
   value = 0;
-  for (int i=0; i<2; i++) {
-    for (int j=0; j<3; j++) {
-      for (int k=0; k<5; k++) {
-        for (int l=0; l<7; l++) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 5; k++) {
+        for (int l = 0; l < 7; l++) {
           value++;
           sgpt_set_i32_4d(a4, i, j, k, l, value);
         }
@@ -130,10 +131,10 @@ void test_set_i32(void) {
     }
   }
   value = 0;
-  for (int i=0; i<2; i++) {
-    for (int j=0; j<3; j++) {
-      for (int k=0; k<5; k++) {
-        for (int l=0; l<7; l++) {
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < 5; k++) {
+        for (int l = 0; l < 7; l++) {
           value++;
           TEST_CHECK(sgpt_get_i32_4d(a4, i, j, k, l) == value);
         }
@@ -145,8 +146,8 @@ void test_set_i32(void) {
 void test_dup_tensor(void) {
   uint8_t mem_buffer[1024];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 1024,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 1024,
+      .mem_buffer = (void*)mem_buffer,
   });
   const sgpt_tensor* a = sgpt_new_tensor_1d(ctx, SGPT_TYPE_F32, 4);
   const sgpt_tensor* b = sgpt_dup_tensor(ctx, a);
@@ -164,14 +165,14 @@ void test_dup_tensor(void) {
 void test_view_tensor(void) {
   uint8_t mem_buffer[1024];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 1024,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 1024,
+      .mem_buffer = (void*)mem_buffer,
   });
-  
+
   const sgpt_tensor* a = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
-  int32_t values[2] = { 2, 3 };
+  int32_t values[2] = {2, 3};
   memcpy(a->data, values, sizeof(int32_t) * 2);
-  
+
   const sgpt_tensor* b = sgpt_view_tensor(ctx, a);
   TEST_CHECK(b->n_dims == 1);
   TEST_CHECK(b->ne[0] == 2);
@@ -190,83 +191,83 @@ void test_view_tensor(void) {
 void test_dup(void) {
   uint8_t mem_buffer[1024];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 1024,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 1024,
+      .mem_buffer = (void*)mem_buffer,
   });
-  
+
   sgpt_tensor* a = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_tensor* b = sgpt_dup(ctx, a);
-  
+
   sgpt_cgraph gf = sgpt_build_forward(b);
   sgpt_set_i32_1d(a, 0, 1);
   sgpt_set_i32_1d(a, 1, 2);
   sgpt_graph_compute(ctx, &gf);
-  
+
   TEST_CHECK(sgpt_get_i32_1d(b, 0) == 1);
-  TEST_CHECK(sgpt_get_i32_1d(b, 1) == 2); 
+  TEST_CHECK(sgpt_get_i32_1d(b, 1) == 2);
 }
 
 void test_dup_inplace(void) {
   uint8_t mem_buffer[1024];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 1024,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 1024,
+      .mem_buffer = (void*)mem_buffer,
   });
-  
+
   sgpt_tensor* a = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_tensor* b = sgpt_dup_inplace(ctx, a);
-  
+
   sgpt_cgraph gf = sgpt_build_forward(b);
   sgpt_set_i32_1d(a, 0, 1);
   sgpt_set_i32_1d(a, 1, 2);
   sgpt_graph_compute(ctx, &gf);
-  
+
   TEST_CHECK(sgpt_get_i32_1d(b, 0) == 1);
-  TEST_CHECK(sgpt_get_i32_1d(b, 1) == 2); 
+  TEST_CHECK(sgpt_get_i32_1d(b, 1) == 2);
 }
 
 void test_add(void) {
   uint8_t mem_buffer[1024];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 1024,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 1024,
+      .mem_buffer = (void*)mem_buffer,
   });
-  
+
   sgpt_tensor* a = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_tensor* b = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_tensor* c = sgpt_add(ctx, a, b);
-  
+
   sgpt_cgraph gf = sgpt_build_forward(c);
   sgpt_set_i32_1d(a, 0, 1);
   sgpt_set_i32_1d(a, 1, 2);
   sgpt_set_i32_1d(b, 0, 3);
   sgpt_set_i32_1d(b, 1, 4);
   sgpt_graph_compute(ctx, &gf);
-  
-  TEST_CHECK(sgpt_get_i32_1d(c, 0) == 1+3);
-  TEST_CHECK(sgpt_get_i32_1d(c, 1) == 2+4); 
+
+  TEST_CHECK(sgpt_get_i32_1d(c, 0) == 1 + 3);
+  TEST_CHECK(sgpt_get_i32_1d(c, 1) == 2 + 4);
 }
 
 void test_add_inplace(void) {
   uint8_t mem_buffer[1024];
   sgpt_context* ctx = sgpt_init((sgpt_init_params){
-    .mem_size = 1024,
-    .mem_buffer = (void*)mem_buffer,
+      .mem_size = 1024,
+      .mem_buffer = (void*)mem_buffer,
   });
-  
+
   sgpt_tensor* a = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_tensor* b = sgpt_new_tensor_1d(ctx, SGPT_TYPE_I32, 2);
   sgpt_tensor* c = sgpt_add_inplace(ctx, a, b);
-  
+
   sgpt_cgraph gf = sgpt_build_forward(c);
   sgpt_set_i32_1d(a, 0, 1);
   sgpt_set_i32_1d(a, 1, 2);
   sgpt_set_i32_1d(b, 0, 3);
   sgpt_set_i32_1d(b, 1, 4);
   sgpt_graph_compute(ctx, &gf);
-  
-  TEST_CHECK(sgpt_get_i32_1d(c, 0) == 1+3);
-  TEST_CHECK(sgpt_get_i32_1d(c, 1) == 2+4); 
+
+  TEST_CHECK(sgpt_get_i32_1d(c, 0) == 1 + 3);
+  TEST_CHECK(sgpt_get_i32_1d(c, 1) == 2 + 4);
 }
 
 TEST_LIST = {
